cd37ae074ac8f094bd8b2cb42ca807b26dd04222
Jurie <16491424@sun.ac.za>
2015-10-27 09:59:26 +0200

    Added tests up to 24

--- run_state_tests.py
+++ run_state_tests.py
@@ -6,6 +6,6 @@ Created on 24 Oct 2015
 
 import subprocess
 
-for i in range (0,20):
+for i in range (1,24):
 	p = subprocess.Popen(['sudo', 'python3', './src/test_state_'+str(i)+'.py', 'False'])
 	p.wait()
